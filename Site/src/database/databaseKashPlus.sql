677233e9e011cb08ecd34cd2b2c903f184a2abd3
VictorRubinec <victor.rubinec@sptech.school>
2022-09-27 19:47:45 -0300

    codigo sql e views

--- Site/src/database/databaseKashPlus.sql
+++ Site/src/database/databaseKashPlus.sql
@@ -21,23 +21,66 @@ idUsuario INT PRIMARY KEY AUTO_INCREMENT
 CREATE TABLE tbMaquina(
 serialNumber INT PRIMARY KEY
 ,fkEmpresa CHAR(14), FOREIGN KEY(fkEmpresa) REFERENCES tbEmpresa(cnpj)
+,nome VARCHAR(100)
 ,cep CHAR(8)
-,modeloCpu VARCHAR(100)
-,nuclesFisicos INT
-,nuclesLogicos INT
-,frequenciaMaxCpu DECIMAL(5,2)
-,tempMaxCpu DECIMAL(5,2)
-,ramMax DECIMAL(5,2)
-,discoMax DECIMAL(7,2)
 );
+
+CREATE TABLE tbComponente(
+idComponente INT PRIMARY KEY AUTO_INCREMENT
+,fkMaquina INT, FOREIGN KEY(fkMaquina) REFERENCES tbMaquina(serialNumber)
+,tipo VARCHAR(100)
+,metrica VARCHAR(100)
+)
+;
 
 CREATE TABLE tbRegistro(
 idRegistro INT PRIMARY KEY AUTO_INCREMENT
-,fkMaquina INT, FOREIGN KEY(fkMaquina) REFERENCES tbMaquina(serialNumber)
-,usoCpu DECIMAL(5,2)
-,tempCpu DECIMAL(5,2)
-,freqCpu DECIMAL(5,2)
-,usoRam DECIMAL(5,2)
-,usoDisco DECIMAL(5,2)
-,dataHorario DATETIME
+,fkComponente INT, FOREIGN KEY(fkComponente) REFERENCES tbComponente(idComponente)
+,registro VARCHAR(50)
+,dataHora DATETIME
 );
+
+-- Criando Views --
+
+CREATE VIEW vwMaquina AS
+    SELECT 
+        tbEmpresa.nome AS 'Empresa'
+        ,tbMaquina.nome AS 'Máquina'
+        ,serialNumber AS 'Número Serial'
+        ,cep AS 'Local (CEP)'
+        ,tipo AS 'Componente'
+    FROM
+        tbEmpresa
+            JOIN
+        tbMaquina ON cnpj = fkEmpresa
+			JOIN
+		tbComponente ON fkMaquina = serialNumber
+;
+        
+CREATE VIEW vwConsumo AS
+    SELECT 
+		idRegistro AS 'ID'
+        ,tbMaquina.nome AS 'Máquina'
+        ,serialNumber AS 'Número Serial'
+        ,tipo AS 'Componente'
+        ,registro AS 'Registro'
+        ,dataHora AS 'Horário'
+    FROM
+        tbMaquina
+            JOIN
+		tbComponente ON fkMaquina = serialNumber
+			JOIN
+		tbRegistro ON fkComponente = idComponente
+;
+
+CREATE VIEW vwFuncionario AS
+    SELECT 
+		tbEmpresa.nome AS 'Máquina'
+		,tbUsuario.nome AS 'Funcionario'
+        ,cargo AS 'Cargo'
+        ,login AS 'Login'
+    FROM
+        tbEmpresa
+            JOIN
+        tbUsuario ON cnpj = fkEmpresa
+;
