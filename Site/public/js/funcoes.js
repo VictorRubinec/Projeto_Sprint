8b26db28c1f3d158e7001ab91e44e9b818505338
ViniScardoso <vinicius.cardoso@sptech.school>
2022-10-20 15:53:13 -0300

    Mapa funcionando

--- Site/public/js/funcoes.js
+++ Site/public/js/funcoes.js
@@ -97,6 +97,7 @@ function listarCaixas() {
 
                         if (window.location == "http://localhost:3333/dashboard/index.html") { // verificando se está na index ou não
                             link.id = serialNumber;
+                            link.focus = cep
                         }
                         else {
                             link.href = "index.html"
@@ -124,6 +125,7 @@ function listarCaixas() {
                     obterDadosGraficoRam(caixas.serialNumber[0]);
                     obterDadosGraficoDisco(caixas.serialNumber[0]);
                     setRamMax(caixas.serialNumber[0]);
+                    recuperarDados(caixas.cep[0]);
                 }
 
             });
@@ -131,7 +133,6 @@ function listarCaixas() {
 
 
         } else {
-
             console.log("Houve um erro ao tentar listar os caixas!");
 
             resposta.text().then(texto => {
@@ -146,11 +147,87 @@ function listarCaixas() {
     return false;
 }
 
+function recuperarDados(cep) {
+
+    if (cep != "") {
+        var script = document.createElement('script');
+        script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=performGeocodingRequest';
+        document.body.appendChild(script);
+    } else {
+        alert("CEP vazio")
+    }
+
+}
+
+
+function performGeocodingRequest(conteudo) {
+    const APIKEY = "2e20e7f8-9be2-4e04-a213-75b2a2040023";
+
+    var logradouro = conteudo.logradouro
+    var bairro = conteudo.bairro
+    var localidade = conteudo.localidade
+
+    var text = `${logradouro}, ${bairro}, ${localidade}`
+
+    // Set URL method depends
+
+    URL = `https://api.mymappi.com/v2/geocoding/direct?apikey=${APIKEY}&q=${text}&country_code=BR`;
+
+    // Create HTTP Request
+    var xmlHttp = new XMLHttpRequest();
+    // Send the request
+    xmlHttp.open("GET", URL, false);
+    // Check Status response
+    xmlHttp.onload = function () {
+        // Update variable, it shows in HTML response
+        response_status = xmlHttp.status;
+        // Bad request != 200 :(
+        if (xmlHttp.status !== 200) {
+            console.log(`Response code ${response_status}. Check your browser log`)
+        } else {
+            console.log(`Response code ${response_status}. Check your browser log`);
+        }
+    };
+    // Close HTML Request
+    xmlHttp.send(null);
+    // Return data field from response to show on result section
+    dados = JSON.parse(xmlHttp.responseText).data;
+    console.log(JSON.stringify(dados))
+    // return JSON.parse(xmlHttp.responseText).data;
+
+    var lat = dados[0].lat;
+    var lon = dados[0].lon;
+
+    gerarMapa(lat, lon);
+
+}
+
+function gerarMapa(latitude, longitude) {
+
+    document.getElementById('mapaCaixa').remove();
+
+    var containerMapa = document.getElementById('container_mapa');
+
+    var mapa = document.createElement('div');
+    mapa.id = "mapaCaixa";
+
+    containerMapa.appendChild(mapa)
+
+    var map = L.map('mapaCaixa').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup('')
+}
+
 
 function addListeners() {
     var caixas = JSON.parse(sessionStorage.INFO_CAIXA);
 
-    var numberReference = caixas.nome; // variável para pegaro length e manipular a quantidade de caixas cadastrados
+    var numberReference = caixas.nome; // variável para pegar o length e manipular a quantidade de caixas cadastrados
 
     for (var i = 0; i < numberReference.length; i++) {
         var serialNumber = caixas.serialNumber[i];
@@ -165,16 +242,14 @@ function addListeners() {
             setRamMax(key);
         })
     }
-}
 
-function gerarMapa(latitude, longitude){
-    var map = L.map('mapaCaixa').setView([-23.704886, -46.564630], 13);
+    for (var i = 0; i < numberReference.length; i++) {
+        var serialNumber = caixas.serialNumber[i];
+        String(serialNumber)
+        document.getElementById(serialNumber).addEventListener("click", function () {
+            cep = this.focus
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    L.marker([-23.704886, -46.564630]).addTo(map)
-        .bindPopup('')
-        .openPopup();
+            recuperarDados(cep)
+        })
+    }
 }
