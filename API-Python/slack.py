3b3b1f020d42be50ec7f45827b7c689541b38f7e
matheuslealdeoliveira <matheus.oliveira@sptech.school>
2022-11-17 11:15:05 -0300

    removendo slack do python

--- API-Python/slack.py
+++
@@ -1,7 +0,0 @@
-from http import client
-from slack_sdk import WebClient
-
-def chamadoSlack(msg, nome):
-    client = WebClient('xoxb-3943229718052-4265288782305-v3XhL7GlCGqD0SeSxibyjHek')
-    response = client.chat_postMessage(channel = 'C0479Q92M4N', text= msg, username= nome)
-
